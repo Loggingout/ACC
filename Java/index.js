8f50dcee53e2307294d10a309194d8517dc8d8a2
INEEDMONEY$ <167272973+Loggingout@users.noreply.github.com>
2025-09-24 18:41:39 +0000

    Changed version of application, fixed navbar toggler icon for smaller screens (Made visable). Update 1.0.0 is live.

--- Java/index.js
+++ Java/index.js
@@ -1,5 +1,5 @@
 function showAlert() {
-    window.alert("Updated 1.0.1 is live ! Check out the new review page.")
+    alert("Update v1.0.0 is live ! Check out the new review page.")
 }
 
 showAlert()
