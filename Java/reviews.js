7c73764d56ca962c03af9e7f1e15193e009277e2
INEEDMONEY$ <167272973+Loggingout@users.noreply.github.com>
2025-12-26 08:02:33 +0000

    🔨Redesign of the menu,catering & reviews section. -Added animations to the menu -Added coffee menu drink images -Added badges for coffee prices and drink availability -Added lunch menu item descriptions -Removed contact navbar item ❌ Contact form will need full-setup meaning, a DB, node, express, render is all needed. Feature is not integrated yet ❌ Leaving comments will need the same full set-up as the contact form. Feature is not integrated yet.

---
+++ Java/reviews.js
@@ -0,0 +1,46 @@
+//Review js file to be used in review.html file
+
+const reviews = [
+    {
+        author: 'Katt B.',
+        rating: 5,
+        comment: 'Sheila is as wonderful a business owner as they come! She is always cheerful, she remembers peoples names, and she makes damn good coffee every time! Specialty beans!!',
+        date: '1 Year ago',
+        source: 'Yelp'
+    },
+    {
+        author: 'Matthew Overstreet',
+        rating: 5,
+        comment: `For those wondering, yes this place is inside the office building, and yes it is well worth you time to find it (it's not hard just follow the signs from the front door). Owner is one of the most cheerful people I've ever met, her energy alone started my day off right, and then I got the coffee! Coffee was amazing, not that bitter bean juice the big chains serve. Support your local coffee shop people!`,
+        date: '1 Year Ago',
+        source: 'Google'
+    },
+    {
+        author: 'Bri W.',
+        rating: 5,
+        comment: 'Great place to stop in for a delicious cup of coffee and a quick snack! Sheila greets everyone with cheer and kindness, truly the light of her business! Serving laughter (free of charge) as she makes your drink, I guarantee you will leave this shop with a smile on your face! :) ',
+        date: '8 Years Ago',
+        source: 'Google'
+    },
+    {
+        author: 'Alexuss',
+        rating: 5,
+        comment: 'This coffee shop is located in the Belcaro building and is definitely the best coffee shop in Denver, the owner is very cheerful and greets everyone with a smile and the atmosphere is very welcoming! Great food and beverages, a great place to grab your morning "pick me up"! Ask about the Chai, its one of my favorites, hot or cold its such a treat!',
+        date: '1 year ago',
+        source: 'Google'
+    },
+    {
+        author: 'Llyod Barta.',
+        rating: 5,
+        comment: `The name is perfect. She is very cheerful. I totally enjoyed the service and the coffee. I'm definitely returning.`,
+        date: '1 year ago',
+        source: 'Google'
+    },
+    {
+        author: 'Denise A.',
+        rating: 5,
+        comment: 'I love Sheila and her energy that she brings to every customer. I would say her cafe has it all. ',
+        date: '8 months ago',
+        source: 'google'
+    },
+]
